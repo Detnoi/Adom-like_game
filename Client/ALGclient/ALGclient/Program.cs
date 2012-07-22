bd306844d4d61bca64bcaecd06c077a339dd9b09
Sergey <kravchenko2508@gmail.com>
2012-07-22 17:42:36 +0300

    Initial client commit
    Actually not doing anythign except connection to the server.

---
+++ Client/ALGclient/ALGclient/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ALGclient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] remdata = new byte[1024];
+            TcpClient Client = new TcpClient();
+            Socket Sock;
+            Client.Connect("localhost", 8020);
+            Sock = Client.Client;
+            Sock.Send(Encoding.ASCII.GetBytes("test\r\n\r\n"));
+            Console.ReadLine();
+            Sock.Disconnect(true);
+        }
+    }
+}
